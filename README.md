b238870c0076b2a1c4255a6358cc61138cf7102c
keshab <self@keshab.net>
2019-11-09 23:12:34 +0100

    batman

---
+++ README.md
@@ -0,0 +1,18 @@
+# go-shell
+
+Writing a simple shell in Go from
+https://sj14.gitlab.io/post/2018/07-01-go-unix-shell/
+
+## Quickstart
+
+Run `go run go-shell.go` It will show a prompt:
+
+```text
+go run go-shell
+> ls
+go-shell.go README.md
+> echo "HELLO"
+"HELLO"
+```
+
+Type `exit` to quit.
---
+++ go-shell.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func execInput(input string) error {
+	input = strings.TrimSuffix(input, "\n")
+
+	args := strings.Split(input, " ")
+
+	switch args[0] {
+	case "cd":
+		if len(args) < 2 {
+			return errors.New("path required")
+		}
+		return os.Chdir(args[1])
+	case "exit":
+		os.Exit(0)
+	}
+
+	cmd := exec.Command(args[0], args[1:]...)
+
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+
+	return cmd.Run()
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("> ")
+
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+
+		if err = execInput(input); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}
+}
